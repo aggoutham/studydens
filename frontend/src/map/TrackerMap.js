6850f1a452f13155a990112497b4baf120de5ace
Shubhashri Annathur Govindarajan <hanachan@Shubhashris-MacBook-Pro.local>
2023-04-01 16:56:36 -0400

    multiple markers in Google Maps: not working

--- frontend/src/map/TrackerMap.js
+++ frontend/src/map/TrackerMap.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 import './TrackerMap.css';
 const TrackerMap = () => {
-
+  const currImage = "http://maps.google.com/mapfiles/ms/icons/blue-dot.png";
   const [currentPosition, setCurrentPosition] = useState({});
   
   const success = position => {
@@ -12,6 +12,59 @@ const TrackerMap = () => {
     }
     setCurrentPosition(currentPosition);
   };
+
+  const studyPlaces = [
+    {
+      name: "Loc 1",
+      location: { 
+        lat: 41.3954,
+        lng: -76 
+      },
+    },
+    {
+      name: "Loc 2",
+      location: { 
+        lat: 40.3917,
+        lng: -78
+      },
+    },
+    {
+      name: "Loc 3",
+      location: { 
+        lat: 40.89,
+        lng: -77.89
+      },
+    },
+    {
+      name: "Loc 4",
+      location: { 
+        lat: 40,
+        lng: -77.67
+      },
+    },
+    {
+      name: "Loc 5",
+      location: { 
+        lat: 41.4055,
+        lng: -78.2
+      },
+    }
+  ];
+
+  var studyPlaceIcons = studyPlaces.map(item => {
+   // console.log(item.name);
+   return(
+      <Marker key={item.name} position={item.location} icon={currImage}/>
+    // <p>Goutham</p>
+   ) })
+
+   var studyHTMLTags = ""
+   for (let i = 0; i < studyPlaceIcons.length; i++) {
+    studyHTMLTags += studyPlaceIcons[i] + "\n";
+    }
+
+
+    console.log("hell:", studyHTMLTags);
   
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(success);
@@ -21,10 +74,8 @@ const TrackerMap = () => {
     height: "100vh",
     width: "100%"};
   
-  const defaultCenter = {
-    lat: 41.3851, lng: 2.1734
-  }
-  console.log(currentPosition.lat);
+  console.log(currentPosition.lat, currentPosition.lng);
+ 
   return (
      <LoadScript
        googleMapsApiKey='AIzaSyC49_JDuSadpV6zVkRORRELrbM_JAG2bxA'>
@@ -32,12 +83,13 @@ const TrackerMap = () => {
           mapContainerStyle={mapStyles}
           zoom={13}
           center={currentPosition}>
-                   {
+          {/* {
             currentPosition.lat &&
             ( 
-              <Marker position={currentPosition}/>
+              <Marker position={currentPosition} icon= {currImage}/>
             ) 
-          }
+          } */}
+          {studyPlaceIcons}
         </GoogleMap>
      </LoadScript>
   )

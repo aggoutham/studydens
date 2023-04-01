7bf02e21ba9a08dffc75860ae24fc9b8b303f673
A G Goutham <aggoutham@gmail.com>
2023-03-31 23:30:51 -0400

    Basic React components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {loggedIn: true, signUpForm: false, signedInUser: ""};
+  }
+
+  signUp = () => {
+
+  };
+
+  loginForm = () => {
+    
+  };
+
+  render() {
+
+    var auth_flow, student_id;
+
+    if(this.state.loggedIn === true){
+      auth_flow = (<div>
+      </div>)
+    } else{
+      if(this.state.signUpForm === true){
+        auth_flow = this.signUp();
+      }
+      else{
+        auth_flow = this.loginForm();
+      }
+    }
+
+    if(this.state.loggedIn === true){
+      student_id = (<p className='right-button'>Hi, {this.state.signedInUser}!</p>)
+    }
+
+    return (
+      <div>
+        <div>
+          <header className="App-header">
+            <p>
+              STUDY DENS
+            </p>
+            {student_id}
+          </header>
+          <button name="logout" className='right-button' onClick={this.handleLogout}>Log Out</button>
+
+        </div>
+      <div className="App">
+        {auth_flow}
+      </div>
+      </div>
+      
+    );
+  };
+
 }
 
 export default App;

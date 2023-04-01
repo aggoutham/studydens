35007b7675c6bcfb433f65bcd47dfbc843de0574
A G Goutham <aggoutham@gmail.com>
2023-04-01 00:06:15 -0400

    boilerplate for sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import { render } from '@testing-library/react';
 import React, { Component } from 'react';
 import './App.css';
+import Sidebar from './sidebar/Sidebar';
 
 class App extends React.Component {
 
+
   constructor(props) {
     super(props);
-    this.state = {loggedIn: true, signUpForm: false, signedInUser: ""};
+    this.state = {loggedIn: true, signUpForm: false, signedInUser: "", showsidebar: false};
   }
 
   signUp = () => {
@@ -17,9 +19,13 @@ class App extends React.Component {
     
   };
 
+  defaultClosedSidebar = () => {
+    return(<p>SideBar</p>);
+  };
+
   render() {
 
-    var auth_flow, student_id;
+    var auth_flow, student_id, sidebar;
 
     if(this.state.loggedIn === true){
       auth_flow = (<div>
@@ -34,7 +40,13 @@ class App extends React.Component {
     }
 
     if(this.state.loggedIn === true){
-      student_id = (<p className='right-button'>Hi, {this.state.signedInUser}!</p>)
+      student_id = (<p className='right-button'>Hi, {this.state.signedInUser}!</p>);
+    }
+
+    if(this.state.showsidebar === true){
+      sidebar = (<Sidebar signedInUser={this.state.signedInUser}></Sidebar>)
+    }else{
+      sidebar = this.defaultClosedSidebar();
     }
 
     return (
@@ -50,6 +62,9 @@ class App extends React.Component {
 
         </div>
       <div className="App">
+        {sidebar}
+      </div>
+      <div className="App">
         {auth_flow}
       </div>
       </div>
@@ -58,5 +73,4 @@ class App extends React.Component {
   };
 
 }
-
 export default App;
---
+++ frontend/src/sidebar/Sidebar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Sidebar extends React.Component {
+
+
+    render (){
+        return(
+            <div className='section-container'>
+                <p className='section-title'>
+                    Expanded Side Bar
+                </p>
+            </div>
+        )
+    }
+
+}
+
+export default Sidebar;

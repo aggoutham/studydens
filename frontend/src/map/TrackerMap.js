1d7c6f4743e0cd5a23bfcfbf64cd3690c8444dd7
Shubhashri Annathur Govindarajan <hanachan@Shubhashris-MacBook-Pro.local>
2023-04-01 03:45:44 -0400

    basic Google Maps with current GPS location

--- frontend/src/map/TrackerMap.js
+++ frontend/src/map/TrackerMap.js
@@ -1,18 +1,45 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 import './TrackerMap.css';
+const TrackerMap = () => {
 
-class TrackerMap extends React.Component {
-
-
-    render (){
-        return(
-            <div>
-                <p>Hello</p>
-                
-            </div>
-        )
+  const [currentPosition, setCurrentPosition] = useState({});
+  
+  const success = position => {
+    const currentPosition = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
     }
+    setCurrentPosition(currentPosition);
+  };
+  
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(success);
+  }, [])
 
+  const mapStyles = {        
+    height: "100vh",
+    width: "100%"};
+  
+  const defaultCenter = {
+    lat: 41.3851, lng: 2.1734
+  }
+  console.log(currentPosition.lat);
+  return (
+     <LoadScript
+       googleMapsApiKey='AIzaSyC49_JDuSadpV6zVkRORRELrbM_JAG2bxA'>
+        <GoogleMap
+          mapContainerStyle={mapStyles}
+          zoom={13}
+          center={currentPosition}>
+                   {
+            currentPosition.lat &&
+            ( 
+              <Marker position={currentPosition}/>
+            ) 
+          }
+        </GoogleMap>
+     </LoadScript>
+  )
 }
-
 export default TrackerMap;
